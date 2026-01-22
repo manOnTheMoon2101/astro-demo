d8c2cfceaca9f9a499d02fbdc0a378c920bd65e0
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2026-01-22 20:06:39 +0200

    svelte form test

---
+++ src/components/Form.svelte
@@ -0,0 +1,34 @@
+
+
+
+<div>
+
+<div>
+    Get In Touch
+</div>
+
+
+<div>
+    <form> 
+        {name}
+    <input placeholder="Your Name" bind:value={name}/>
+    <input placeholder="Your Name"/>
+    <input placeholder="Your Name"/>
+    <input placeholder="Your Name"/>
+
+    <textarea placeholder="Tell us about you project"></textarea>
+
+
+    <button>Send Message</button>
+</form>
+</div>
+
+</div>
+
+
+<script>
+
+
+let name = $state()
+
+</script>
