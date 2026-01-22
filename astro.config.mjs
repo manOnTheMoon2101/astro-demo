473a1aa8712d61ed3c1949f98f60e650d1a29b98
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2026-01-22 20:27:46 +0200

    server fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
